71aff961359ce40bb7618242cf467a29d1b6f6a7
vivian waithera <vivyannekori@gmail.com>
2023-06-20 18:08:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy' , 'Sally' , 'Annette' , 'Sarah' , 'Bobby']
+
+function findMatching(name, drivers) {
+    return name.filter(
+      (matchingNames) => matchingNames.toLowerCase() === drivers.toLowerCase()
+    );
+}
+
+function fuzzyMatch(name, firstLetter){
+    return name.filter(
+        matchingNames => matchingNames.toLowerCase().indexOf(firstLetter.toLowerCase()) === 0
+    )
+}
+
+function matchName (person, propertiesByName) {
+    return person.filter(
+        details => details.name === propertiesByName
+    )
+}
